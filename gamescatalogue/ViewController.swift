0c41405cea12121e9fd17b9f540708f51c542366
AlfaCentaury <alfa.arnialfa@gmail.com>
2021-08-27 21:29:34 +0700

    Initial Commit

---
+++ gamescatalogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gamescatalogue
+//
+//  Created by Alfa Centaury on 27/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
